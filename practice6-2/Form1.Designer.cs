0aa61e1155e4a35a7ffa6feca2f162a79670ae00
PeiKuo <55939462+psh09018@users.noreply.github.com>
2020-09-01 17:31:55 +0800

    First Upload

---
+++ practice6-2/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace practice6_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ship = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.ship)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ship
+            // 
+            this.ship.Location = new System.Drawing.Point(175, 421);
+            this.ship.Name = "ship";
+            this.ship.Size = new System.Drawing.Size(70, 70);
+            this.ship.TabIndex = 1;
+            this.ship.TabStop = false;
+            this.ship.Tag = "shipp";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 60F);
+            this.label2.Location = new System.Drawing.Point(452, 194);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(272, 100);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Silver;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(450, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(135, 520);
+            this.panel1.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.CausesValidation = false;
+            this.label4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label4.Font = new System.Drawing.Font("Tw Cen MT Condensed Extra Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(19, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 25);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "RESTART";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微軟正黑體", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(38, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 24);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(7, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Score:";
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(574, 503);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ship);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ShipMove);
+            ((System.ComponentModel.ISupportInitialize)(this.ship)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox ship;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ practice6-2/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practice6_2
+{
+    public partial class Form1 : Form
+    {
+        PictureBox invader = new PictureBox();
+        PictureBox ball = new PictureBox();
+        int counter = 0;
+        int score = 0;
+        bool start = true;
+        bool playing = true;
+        bool reset = false;
+        public Form1()
+        {
+            InitializeComponent();
+            ship.Image = new Bitmap(Properties.Resources.ship_1, ship.Size);
+            ship.Width = 53;
+            ship.Height = 53;
+            ball.Width = 20;
+            ball.Height = 20;
+            invader.Image = new Bitmap(Properties.Resources.ship_1, ship.Size);
+            ball.Image = new Bitmap(Properties.Resources.pp, ball.Size);
+            //timer1.Enabled = true;
+            //timer2.Enabled = true;
+            timer1.Start();
+            timer2.Start();
+        }
+
+        private void ShipMove(object sender, KeyEventArgs e)
+        {
+            if (start==true)
+            {
+                switch (e.KeyCode)
+                {
+                    case Keys.Left:
+                        if (ship.Left >= 2)
+                            ship.Left -= 5;
+                        break;
+                    case Keys.Right:
+                        if (ship.Left <= 283)
+                            ship.Left += 5;
+                        break;
+                    case Keys.Down:
+                        if (ship.Top <= 343)
+                            ship.Top += 5;
+                        break;
+                    case Keys.Up:
+                        if (ship.Top >= 2)
+                            ship.Top -= 5;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        Random rnd = new Random();
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            foreach (Control inv in this.Controls)
+            {
+                if (inv is PictureBox && inv.Tag == "invad")
+                {
+                    inv.Top += 5;
+                    if (inv.Top > this.ClientSize.Height)
+                    {
+                        invader.Image = Properties.Resources.Invader;
+                        invader.Location = inv.Location;
+                        invader.Height = 70;
+                        invader.Width = 70;
+                        invader.BackColor = Color.Transparent;
+
+                        inv.Top = 0;
+                        inv.Left = rnd.Next(0, 290);
+                    }
+                    if (inv.Bounds.IntersectsWith(ship.Bounds))
+                    {
+                        timer1.Stop();
+                        timer2.Stop();
+                        start = false;
+                        playing = false;
+                    }
+                }
+                if (inv is PictureBox && inv.Tag == "ball")
+                {
+                    inv.Top += 10;
+                    if (inv.Top > this.ClientSize.Height)
+                    {
+                        inv.Dispose();
+                    }
+                    if (inv.Bounds.IntersectsWith(ship.Bounds))
+                    {
+                        inv.Dispose();
+                        score += 200;
+                    }
+                }
+
+                counter++;
+                if (counter == 10)
+                {
+                    PictureBox invader = new PictureBox();
+                    invader.Height = 53;
+                    invader.Width = 53;
+                    invader.Top = 0;
+                    invader.Left = 10;
+                    invader.Image = Properties.Resources.Invader;
+                    invader.Tag = "invad";
+                    this.Controls.Add(invader);
+                }
+                if (counter%250 == 0)
+                {
+                    PictureBox ball = new PictureBox();
+                    ball.Height = 20;
+                    ball.Width = 20;
+                    ball.Top = 0;
+                    ball.Left = rnd.Next(0,320);
+                    ball.Image = Properties.Resources.pp;
+                    ball.Tag = "ball";
+                    this.Controls.Add(ball);
+                }
+                if (counter == 200)
+                {
+                    PictureBox invader = new PictureBox();
+                    invader.Height = 53;
+                    invader.Width = 53;
+                    invader.Top = 0;
+                    invader.Left = 100;
+                    invader.Image = Properties.Resources.Invader;
+                    invader.Tag = "invad";
+                    this.Controls.Add(invader);
+                }
+                if (counter == 400)
+                {
+                    PictureBox invader = new PictureBox();
+                    invader.Height = 53;
+                    invader.Width = 53;
+                    invader.Top = 0;
+                    invader.Left = 200;
+                    invader.Image = Properties.Resources.Invader;
+                    invader.Tag = "invad";
+                    this.Controls.Add(invader);
+                }
+                if (counter == 600)
+                {
+                    PictureBox invader = new PictureBox();
+                    invader.Height = 53;
+                    invader.Width = 53;
+                    invader.Top = 0;
+                    invader.Left = 290;
+                    invader.Image = Properties.Resources.Invader;
+                    invader.Tag = "invad";
+                    this.Controls.Add(invader);
+                }
+
+                /*
+                if (counter==3)
+                {
+                    PictureBox invader;
+                    invader = new PictureBox();
+                    invader.Image = new Bitmap(Properties.Resources.ship_1, ship.Size);
+                    invader_x = rnd.Next(0, 285);
+                    invader.Location = new Point(invader_x, 0);
+                    invader.BackColor = Color.Transparent;
+                    this.Controls.Add(invader);
+                    counter = 0;
+                }
+                else {
+                    counter++;
+                }
+                foreach (Control invader in this.Controls) {
+
+                }
+                */
+
+            }
+        }
+
+        private void restart() {
+            if (!playing)
+            {
+                foreach (Control inv in this.Controls)
+                {
+                    for (int i = this.Controls.Count - 1; i >= 0; i--)
+                    {
+                        if (this.Controls[i] is PictureBox)
+                            this.Controls[i].Dispose();
+                    }
+                }
+                ship = new PictureBox();
+                ship.Width = 53;
+                ship.Height = 53;
+                ship.Image = new Bitmap(Properties.Resources.ship_1, ship.Size);
+                ship.Top = 300;
+                ship.Left = 150;
+                this.Controls.Add(ship);
+                start = true;
+                playing = true;
+                reset = true;
+                timer1.Start();
+                start = true;
+                reset = false;
+                timer2.Start();
+                score = 0;
+                counter = 0;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            start = true;
+            restart();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            restart();
+        }
+
+        //private void timer2_Tick(object sender, EventArgs e)
+        //{
+        //    //counter2++;
+        //}
+
+        private void timer2_Tick_1(object sender, EventArgs e)
+        {
+            score++;
+            label3.Text = score.ToString();
+        }
+    }
+}
